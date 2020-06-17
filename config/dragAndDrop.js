7fd003e1106d46977c92849990a02b5d0ba19f9e
caggarwa <49152543+caggarwa@users.noreply.github.com>
2020-06-17 16:04:33 +0530

    Create dragAndDrop.js

---
+++ config/dragAndDrop.js
@@ -0,0 +1,37 @@
+function createEvent(typeOfEvent) {
+	console.log("---------------CreateEvent---------------");
+    var event = document.createEvent("CustomEvent");
+    event.initCustomEvent(typeOfEvent, true, true, null);
+    event.dataTransfer = {
+        data: {},
+        setData: function (key, value) {
+            this.data[key] = value;
+        },
+        getData: function (key) {
+            return this.data[key];
+        }
+    };
+    return event;
+}
+function dispatchEvent(element, event, transferData) {
+console.log("---------------DispatchEvent---------------");
+    if (transferData !== undefined) {
+        event.dataTransfer = transferData;
+    }
+    if (element.dispatchEvent) {
+        element.dispatchEvent(event);
+    } else if (element.fireEvent) {
+        element.fireEvent("on" + event.type, event);
+    }
+}
+function simulateHTML5DragAndDrop(element, target) {
+console.log("---------------Drag and drop---------------");
+    var dragStartEvent = createEvent('dragstart');
+    dispatchEvent(element, dragStartEvent);
+    var dropEvent = createEvent('drop');
+    dispatchEvent(target, dropEvent, dragStartEvent.dataTransfer);
+    var dragEndEvent = createEvent('dragend');
+    dispatchEvent(element, dragEndEvent, dropEvent.dataTransfer);
+    
+} 
+
